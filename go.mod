21a0f15ce5d694f1e2613dc505ab090c3cb297e2
Kelly Norton <kellegous@gmail.com>
2025-04-22 21:53:21 -0400

    add store stats

--- go.mod
+++ go.mod
@@ -17,5 +17,6 @@ require (
 	github.com/kellegous/buildname v0.2.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/sys v0.31.0 // indirect
 )
--- internal/web/server.go
+++ internal/web/server.go
@@ -2,13 +2,18 @@ package web
 
 import (
 	"context"
+	"net"
 	"net/http"
+	"sync"
+	"time"
+
+	"github.com/twitchtv/twirp"
+	"golang.org/x/sync/errgroup"
+	"google.golang.org/protobuf/types/known/emptypb"
 
 	"github.com/kellegous/sonar"
 	"github.com/kellegous/sonar/internal/config"
 	"github.com/kellegous/sonar/internal/store"
-	"github.com/twitchtv/twirp"
-	"google.golang.org/protobuf/types/known/emptypb"
 )
 
 type Server struct {
@@ -52,5 +57,48 @@ func (s *Server) GetHourly(ctx context.Context, req *emptypb.Empty) (*sonar.GetH
 }
 
 func (s *Server) GetStoreStats(ctx context.Context, req *emptypb.Empty) (*sonar.GetStoreStatsResponse, error) {
+	var lock sync.Mutex
+	earliest := store.Last
+	latest := store.First
+	var count int64
+	g, _ := errgroup.WithContext(ctx)
+	for _, host := range s.Config.Hosts {
+		host := host
+		g.Go(func() error {
+			var a, b time.Time
+			var c int64
+			if err := s.Store.ForEach(
+				store.NewMarker(host.IP, store.First),
+				store.NewMarker(host.IP, store.Last),
+				func(ip net.IP, t time.Time, vals []time.Duration) error {
+					if a.IsZero() || t.Before(a) {
+						a = t
+					}
+					if b.IsZero() || t.After(b) {
+						b = t
+					}
+					c += int64(len(vals))
+					return nil
+				}); err != nil {
+				return err
+			}
+
+			lock.Lock()
+			defer lock.Unlock()
+			if a.Before(earliest) {
+				earliest = a
+			}
+			if b.After(latest) {
+				latest = b
+			}
+			count += c
+			return nil
+		})
+	}
+
+	if err := g.Wait(); err != nil {
+		return nil, twirp.InternalErrorWith(err)
+	}
+
 	return nil, twirp.NewError(twirp.Unimplemented, "not implemented")
 }
